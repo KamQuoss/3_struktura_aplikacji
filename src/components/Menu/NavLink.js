3178e52ea72f829fab6df25c66e74b96cdc8e233
KamQuoss <51073261+KamQuoss@users.noreply.github.com>
2021-11-30 11:44:45 -0800

    initial commit

--- src/components/Menu/NavLink.js
+++ src/components/Menu/NavLink.js
@@ -1,27 +1,29 @@
 import React from "react";
 
 const styles = {
-    display: 'block',
-    padding: '8px 16px',
-    textTransform: 'uppercase',
-    background: 'lightgray',
-    borderBottom: '1px solid gray'
-}
-
-const linkStyles = {
-    textDecoration: 'none',
-}
-const activeLinkStyles = {
-    textDecoration: 'none',
-    color: 'red'
-}
-
-function MenuLink({ to = '#', isActive, children }) {
-    return (
-        <li style={styles}>
-            <a href={to} style={isActive ? activeLinkStyles : linkStyles}>{children}</a>
-        </li>
-    )
+  display: "block",
+  padding: "8px 16px",
+  textTransform: "uppercase",
+  background: "lightgray",
+  borderBottom: "1px solid gray"
 };
 
-export default MenuLink;
+const linkStyles = {
+  textDecoration: "none"
+};
+const activeLinkStyles = {
+  textDecoration: "none",
+  color: "red"
+};
+
+function NavLink({ to = "#", isActive, children }) {
+  return (
+    <li style={styles}>
+      <a href={to} style={isActive ? activeLinkStyles : linkStyles}>
+        {children}
+      </a>
+    </li>
+  );
+}
+
+export default NavLink;
